0480f7bc75cbe5d09cdf5042eae1e4dea9d31676
David Wright <dontbothertheking@gmail.com>
2020-02-11 13:30:00 -0600

    Add a test for fileImports

--- ScriptExtracter.Tests/Extract_Test.fs
+++ ScriptExtracter.Tests/Extract_Test.fs
@@ -20,3 +20,9 @@ let ``getFiles returns all the files`` () =
 let ``getDotNames correcty names files`` () =
     Assert.Equal("one", Extract.getDotName "one")
     Assert.Equal("one.two.three", Extract.getDotName "one/two/three")
+
+[<Theory>]
+[<InlineData("""require("main.js")""", """require("main.js")""")>]
+[<InlineData("""require("./main.js")""", """require("main.js")""")>]
+let ``fixImports correctly changes imports`` (expected: string) (actual: string) =
+    Assert.Equal(Extract.fixImports expected, actual)
