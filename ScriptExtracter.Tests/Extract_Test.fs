253e60445fe0ce26040962129bd078044b8b9807
David Wright <dontbothertheking@gmail.com>
2020-02-12 18:45:36 -0600

    Add util tests

--- ScriptExtracter.Tests/Extract_Test.fs
+++ ScriptExtracter.Tests/Extract_Test.fs
@@ -7,16 +7,6 @@ open System.Collections
 
 let testFolderPath = "TestFolder"
 
-[<Fact>]
-let ``getFiles returns all the files`` () =
-    let discoverdFiles = Extract.getFiles testFolderPath |> Seq.length
-    Assert.Equal(3, discoverdFiles)
-
-[<Fact>]
-let ``getDotNames correcty names files`` () =
-    Assert.Equal("one", Extract.getDotName "one")
-    Assert.Equal("one.two.three", Extract.getDotName "one/two/three")
-
 [<Theory>]
 [<InlineData("""require("main.js")""", """require("main.js")""")>]
 [<InlineData("""require("./main.js")""", """require("main.js")""")>]
---
+++ ScriptExtracter.Tests/Util_Tests.fs
@@ -0,0 +1,15 @@
+﻿module Util_Tests
+open Xunit
+
+let testFolderPath = "TestFolder"
+
+[<Fact>]
+let ``getFiles returns all the files`` () =
+    let discoverdFiles = Util.getFiles testFolderPath |> Seq.length
+    Assert.Equal(3, discoverdFiles)
+
+[<Fact>]
+let ``getDotNames correcty names files`` () =
+    Assert.Equal("one", Util.getDotName "one")
+    Assert.Equal("one.two.three", Util.getDotName "one/two/three")
+
