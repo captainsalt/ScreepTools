f362541e7b4d12d15d885f4232b3ce873e49b140
David Wright <dontbothertheking@gmail.com>
2020-02-13 22:19:04 -0600

    Remove fileOps

--- ScriptExtracter/Types.fs
+++ ScriptExtracter/Types.fs
@@ -9,16 +9,3 @@ type FileRecord = {
     dotName: string
     dotFullPath: string
 }
-
-type IFileOps = 
-    abstract WriteToFile: path: string -> text: string -> Async<unit>
-    abstract DeleteFile: path: string -> unit
-
-type FileOps() = 
-    interface IFileOps with
-        member this.WriteToFile path text = async {
-            do! File.WriteAllTextAsync(path, text) |> Async.AwaitTask
-        }
-
-        member this.DeleteFile path = 
-            File.Delete(path)
