9e28e3195c78a781bfb7327d29b553809d1ded14
David Wright <dontbothertheking@gmail.com>
2020-02-14 10:58:59 -0600

    Update fileImports test

--- ScriptExtracter.Tests/Extract_Test.fs
+++ ScriptExtracter.Tests/Extract_Test.fs
@@ -10,12 +10,12 @@ open System.IO.Abstractions.TestingHelpers
 let ``fixImports correctly changes imports`` () =
     let sourcePath = @"C:\js\"
     let targetPath = @"C:\target"
-    let subFileText = "require(\"./../main.js\")"
+    let subFilePath = @"C:\js\subfolder\subfile.js"
 
     let mockFs = 
         let fileSystem = MockFileSystem()
         fileSystem.AddFile(@"C:\js\main.js", new MockFileData(""))
-        fileSystem.AddFile(@"C:\js\subfolder\subfile.js", new MockFileData(""))
+        fileSystem.AddFile(@"C:\js\subfolder\subfile.js", new MockFileData("require(\"./../main.js\")"))
         fileSystem
 
     let fileRecords = 
@@ -27,6 +27,6 @@ let ``fixImports correctly changes imports`` () =
         <| targetPath 
         <| testFiles
 
-    let fixedText = Extract.fixImports mockFs fileRecords subFileText  
+    let (fixedText, _) = Extract.fixImports mockFs fileRecords subFilePath |> Async.RunSynchronously  
     let expectedText = "require(\"main.js\")"
     Assert.Equal(expectedText, fixedText)
