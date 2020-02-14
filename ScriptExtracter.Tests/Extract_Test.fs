e78fb263208ba6f228990a4ea1a39c35e67e9304
David Wright <dontbothertheking@gmail.com>
2020-02-14 11:39:16 -0600

    Add test for deleteMissing

--- ScriptExtracter.Tests/Extract_Test.fs
+++ ScriptExtracter.Tests/Extract_Test.fs
@@ -7,7 +7,7 @@ open System.Collections
 open System.IO.Abstractions.TestingHelpers
 
 [<Fact>]
-let ``fixImports correctly changes imports`` () =
+let ``Assert fixImports correctly changes imports`` () =
     let sourcePath = @"C:\js\"
     let targetPath = @"C:\target"
     let subFilePath = @"C:\js\subfolder\subfile.js"
@@ -30,3 +30,30 @@ let ``fixImports correctly changes imports`` () =
     let (fixedText, _) = Extract.fixImports mockFs fileRecords subFilePath |> Async.RunSynchronously  
     let expectedText = "require(\"main.js\")"
     Assert.Equal(expectedText, fixedText)
+
+[<Fact>]
+let ``Assert deleteMissing removes missing files from target directory`` () = 
+    let sourcePath = @"C:\js\"
+    let targetPath = @"C:\target"
+    let subFilePath = @"C:\js\subfolder\subfile.js"
+
+    let mockFs = 
+        let fileSystem = MockFileSystem()
+        fileSystem.AddFile(@"C:\js\noDelete.js", new MockFileData(""))
+
+        fileSystem.AddFile(@"C:\target\delete.js", new MockFileData(""))
+        fileSystem.AddFile(@"C:\target\noDelete.js", new MockFileData(""))
+        fileSystem
+
+    let fileRecords = 
+        let testFiles = Util.getSourceFiles mockFs sourcePath
+
+        Util.generateFileRecords
+        <| mockFs 
+        <| sourcePath 
+        <| targetPath 
+        <| testFiles
+
+    Extract.deleteMissing mockFs fileRecords targetPath
+    Assert.DoesNotContain(@"C:\target\delete.js", mockFs.AllFiles)
+    Assert.Contains(@"C:\target\noDelete.js", mockFs.AllFiles)
