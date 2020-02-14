74e242056391859cd96d09b992d6e7e56ac02d15
David Wright <dontbothertheking@gmail.com>
2020-02-14 11:06:34 -0600

    Update extractFile

--- ScriptExtracter/Extract.fs
+++ ScriptExtracter/Extract.fs
@@ -56,20 +56,19 @@ let fixImports (fs: IFileSystem) (fileRecords: FileRecord seq) filePath = async 
 }
 
 /// Extracts the sourceFile to the target path
-let extractFile (fs: IFileSystem) (fileRecords: FileRecord seq) targetPath sourceFile = async {
-    let! fileText = fs.File.ReadAllTextAsync(sourceFile) |> Async.AwaitTask
+let extractFile (fs: IFileSystem) (fileRecords: FileRecord seq) targetPath sourceFilePath = async {
     let newFilePath = 
         fileRecords 
-        |> Seq.find (fun record -> record.sourceFullPath = sourceFile) 
+        |> Seq.find (fun record -> record.sourceFullPath = sourceFilePath) 
         |> fun record -> record.dotFullPath
 
     if fs.Directory.Exists(targetPath) |> not then
         fs.Directory.CreateDirectory(targetPath) |> ignore
 
-    let fixedText = fixImports fs fileRecords fileText
+    let (fixedText, oldText) = fixImports fs fileRecords sourceFilePath |> Async.RunSynchronously
 
     if fs.File.Exists(newFilePath) then
-        if fileText = fixedText then 
+        if oldText = fixedText then 
             ()
         else
             do! fs.File.WriteAllTextAsync(newFilePath, fixedText) |> Async.AwaitTask
